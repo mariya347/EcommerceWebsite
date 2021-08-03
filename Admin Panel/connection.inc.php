097a9cc9f314e7ba21e2b3f6d40238e39b3864b0
mariya347 <fmariya245@gmail.com>
2021-08-03 20:40:35 +0500

    change path

--- Admin Panel/connection.inc.php
+++ Admin Panel/connection.inc.php
@@ -2,7 +2,7 @@
 session_start();
 $con=mysqli_connect("remotemysql.com","FByoewkuCV","0tf3smZiAu","FByoewkuCV");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/webmakeup/');
-define('SITE_PATH','http://127.0.0.1/webmakeup/');
+define('SITE_PATH','https://mymakeupcity.herokuapp.com/webmakeup/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
