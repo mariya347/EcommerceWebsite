997726231325a3c6ed7a5c370996756856d810cb
mariya347 <fmariya245@gmail.com>
2021-08-03 20:19:48 +0500

    added remote mysql connection

--- Admin Panel/connection.inc.php
+++ Admin Panel/connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecom");
+$con=mysqli_connect("remotemysql.com","FByoewkuCV","0tf3smZiAu","FByoewkuCV");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/webmakeup/');
 define('SITE_PATH','http://127.0.0.1/webmakeup/');
 
