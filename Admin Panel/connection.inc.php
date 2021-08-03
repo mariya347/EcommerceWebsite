3e270381c2c5dbe8027e93fcaa8b39dc4a4452f7
mariya347 <fmariya245@gmail.com>
2021-08-03 21:06:18 +0500

    change path

--- Admin Panel/connection.inc.php
+++ Admin Panel/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("remotemysql.com","FByoewkuCV","0tf3smZiAu","FByoewkuCV");
+$con=mysqli_connect("localhost","root","","ecom");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/webmakeup/');
-define('SITE_PATH','http://remotemysql.com/webmakeup/');
+define('SITE_PATH','http://127.0.0.1/webmakeup/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
