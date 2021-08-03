3d2f6f7efa6ac23e9a804a28201531b92843d3ff
mariya347 <fmariya245@gmail.com>
2021-08-03 20:51:27 +0500

    changes

--- connection.inc.php
+++ connection.inc.php
@@ -2,7 +2,7 @@
 session_start();
 $con=mysqli_connect("remotemysql.com","FByoewkuCV","0tf3smZiAu","FByoewkuCV");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/webmakeup');
-define('SITE_PATH','https://mymakeupcity.herokuapp.com/webmakeup/');
+define('SITE_PATH','http://remotemysql.com/webmakeup/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
